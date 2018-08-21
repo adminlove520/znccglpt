ae9a74f4f6f4ab64f4ba7126a8117415bd5ed4b0
xiongyuxiang <xiongyuxiang@zhuojianchina.com>
2018-08-21 11:04:34 +0800

    登录页面样式

---
+++ 智能仓储管理平台/src/main/webapp/js/login.js
@@ -0,0 +1,23 @@
+﻿// 用户登录
+var username;
+var password;
+var type;
+$("#login").click(function (){
+    username = $("#username").val();
+    password = $("#password").val();
+    type = $('input:radio[name="type"]:checked').val();
+    if(username == ""){
+        alert("请输入用户名！");
+    }
+    else if(password == ""){
+        alert("请输入密码！");
+    }
+    else if(type == null){
+        alert("请选择身份权限！");
+    }
+    else{
+        console.log(username);
+        console.log(password);
+        console.log(type);
+    }
+});
